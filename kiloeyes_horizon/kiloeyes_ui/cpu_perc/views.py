a72e031506b6602b1ec71f36f9fc68ee27a809b9
Vishnu Govindaraj <vg249@cornell.edu>
2016-05-13 07:11:37 +0000

    New Horizon Dashboard plugin for Kiloeyes Metrics
    Change-Id: I79a10ef85fc68eb1a2759032e004e79f74d3bcf6

---
+++ kiloeyes_horizon/kiloeyes_ui/cpu_perc/views.py
@@ -0,0 +1,24 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from django.conf import settings
+from horizon import views
+
+
+class IndexView(views.APIView):
+    # A very simple class-based view...
+    template_name = 'kiloeyes_ui/cpu_perc/index.html'
+
+    def get_data(self, request, context, *args, **kwargs):
+        # Add data to the context here...
+        context['kibana_url'] = settings.KIBANA_URL
+        return context
