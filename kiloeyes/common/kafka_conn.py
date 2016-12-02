d7dadc60711028aaf9718c9a3454ea8aa8751764
Mehdi Abaakouk <sileht@sileht.net>
2016-12-02 09:55:00 +0100

    Update to python-kafka>=1.3.1
    The performance issue have been solved:
    
    http://lists.openstack.org/pipermail/openstack-dev/2016-December/108350.html
    https://review.openstack.org/#/c/405587/
    
    Change-Id: Ia54167dcce23b8b1f9d08d2120675fda5f4fbbeb

--- kiloeyes/common/kafka_conn.py
+++ kiloeyes/common/kafka_conn.py
@@ -99,7 +99,7 @@ class KafkaConnection(object):
                     wait_time = self.wait_time
                 time.sleep(wait_time)
 
-                self._client = client.KafkaClient(self.uri)
+                self._client = client.SimpleClient(self.uri)
 
                 # when a client is re-initialized, existing consumer should be
                 # reset as well.
